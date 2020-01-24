411dfe9fb02b49b6cd11d1ec40ba3eaf9a61362e
luckyNB <20150773@dbatu.ac.in>
2020-01-24 09:56:57 +0530

    Added code for screenshots and Listeners in TestNG

---
+++ src/main/java/com/ebookstore/util/CustomListener.java
@@ -0,0 +1,44 @@
+package com.ebookstore.util;
+
+import com.ebookstore.base.BaseClass;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.util.Date;
+
+public class CustomListener extends BaseClass implements ITestListener {
+
+	public void onTestStart(ITestResult result) {
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		Date date=new Date();
+		String imageName="Image"+date.toString();
+		Utility.captureScreenShot(driver,imageName);
+	}
+
+	public void onTestFailure(ITestResult result) {
+		Date date=new Date();
+		String imageName="Image"+date.toString();
+		Utility.captureScreenShot(driver,imageName);
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		
+	}
+
+	public void onStart(ITestContext context) {
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		
+	}
+
+}

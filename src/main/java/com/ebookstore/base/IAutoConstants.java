9e74364a3f747a684428b0ce56eebf6d56286dc5
sonali <blsonalib@gmail.com>
2020-01-25 19:04:53 +0530

    Added json File path

--- src/main/java/com/ebookstore/base/IAutoConstants.java
+++ src/main/java/com/ebookstore/base/IAutoConstants.java
@@ -2,5 +2,5 @@ package com.ebookstore.base;
 
 public class IAutoConstants {
    public static String filePath = "./src/test/resources/TestData/filterBooksTestData.json";
-   public static String filePathForSearch = "./src/test/resources/SearchBookByAuthorOrTitle.json";
+   public static String filePathForSearch = "./src/test/resources/TestData/SearchBookByAuthorOrTitle.json";
 }
